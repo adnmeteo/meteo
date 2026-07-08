c809b780d6eb1821483ff2e658e7e1603a937e68
adnmeteo <admazit@gmail.com>
2026-07-08 22:56:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 // Bienvenue sur ma station météo 🌦️
 
 console.log("Station météo chargée !");
+
+async function chargerDonnees() {
+
+    const reponse = await fetch("data/latest.csv");
+    const texte = await reponse.text();
+
+    console.log(texte);
+
+}
+
+chargerDonnees();
