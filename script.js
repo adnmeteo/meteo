d6b152c66fcc11e0e8a101058e36b68e71bfc9ff
adnmeteo <admazit@gmail.com>
2026-07-09 16:09:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 async function chargerDonnees() {
 
     const reponse = await fetch("data/latest.csv");
-
     const texte = await reponse.text();
 
-    document.getElementById("csv").textContent = texte;
+    const lignes = texte.trim().split("\n");
+
+    let html = "<table>";
+
+    lignes.forEach((ligne, index) => {
+
+        const colonnes = ligne.split(";");
+
+        html += "<tr>";
+
+        colonnes.forEach(colonne => {
+
+            colonne = colonne.replaceAll('"', '');
+
+            if (index === 0) {
+                html += `<th>${colonne}</th>`;
+            } else {
+                html += `<td>${colonne}</td>`;
+            }
+
+        });
+
+        html += "</tr>";
+
+    });
+
+    html += "</table>";
+
+    document.getElementById("tableau").innerHTML = html;
 
 }
 
